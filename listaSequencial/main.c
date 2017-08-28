f3debe5949149edf95a2a092c24a89860226fc12
Gabriel Freitas <gabrieltas@Gabriels-MacBook-Pro.local>
2017-08-28 17:15:10 -0300

    Initial Commit

---
+++ listaSequencial/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  listaSequencial
+//
+//  Created by Gabriel Freitas on 28/08/17.
+//  Copyright © 2017 Gabriel Freitas. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
